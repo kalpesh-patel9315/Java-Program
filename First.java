52c76e4246785001f7774fcb04df1ad09668ca07
kalpesh-patel9315 <kalpeshpatel.ivorytechnolab@gmail.com>
2024-05-31 17:03:07 +0530

    Initial commit

---
+++ First.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class First{
+    public static void main(String args[]){
+    System.out.println("this is a java program..");
+    System.out.println("Output from java program.."+new Date());
+    }
+}
