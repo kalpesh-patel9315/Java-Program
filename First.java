79c61af0874e4e8afacd0a28f96b650da08e4b41
kalpesh-patel9315 <kalpeshpatel.ivorytechnolab@gmail.com>
2024-05-31 17:07:16 +0530

    java program for jenkins

--- First.java
+++ First.java
@@ -4,5 +4,6 @@ class First{
     public static void main(String args[]){
     System.out.println("this is a java program..");
     System.out.println("Output from java program.."+new Date());
+    System.out.println("Hello");
     }
 }
