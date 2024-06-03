39efb26b1062750aaf7c724e564758d0ffe1b882
kalpesh-patel9315 <kalpeshpatel.ivorytechnolab@gmail.com>
2024-06-03 10:49:24 +0530

    delete hello line

--- First.java
+++ First.java
@@ -4,6 +4,5 @@ class First{
     public static void main(String args[]){
     System.out.println("this is a java program..");
     System.out.println("Output from java program.."+new Date());
-    System.out.println("Hello");
     }
 }
