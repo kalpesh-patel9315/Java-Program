c83399e8c23ca423b19f864b40a3243c9abb98c1
kalpesh-patel9315 <kalpeshpatel.ivorytechnolab@gmail.com>
2024-06-03 11:04:29 +0530

    Added one line

--- First.java
+++ First.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 class First{
     public static void main(String args[]){
-    System.out.println("this is a java program..");
-    System.out.println("Output from java program.."+new Date());
+        System.out.println("this is a java program..");
+        System.out.println("Output from java program.."+new Date());
+        System.out.println("Hello java developer..");
     }
 }
